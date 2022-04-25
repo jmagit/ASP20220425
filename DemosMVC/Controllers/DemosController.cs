1eea141223945eb7e832e6c77c707c03b3e96470
Javier <jmartin@grupoloyal.com>
2022-04-25 14:01:39 +0200

    Modelos y Controladores

---
+++ DemosMVC/Controllers/DemosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemosMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemosMVC.Controllers {
+    public class DemosController : Controller {
+        public IActionResult Index() {
+            var dao = new PersonaRepository();
+            Persona item;
+            if (true) {
+                item = dao.Get(1);
+            } else {
+                item = dao.Get(2);
+            }
+            return View();
+        }
+    }
+}
---
+++ DemosMVC/Models/Personas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemosMVC.Models {
+    public class Persona {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; } = DateTime.Now;
+        public DateTime FechaBaja { get; set; }
+        public bool Activo { get; set; } = true;
+
+        public int Edad {
+            get {
+                return DateTime.Now.Year - FechaNacimiento.Year - (DateTime.Now.DayOfYear > FechaNacimiento.DayOfYear ? -1 : 0);
+            }
+        }
+
+        public void Jubilate() {
+            Activo = false;
+            FechaBaja = DateTime.Now;
+        }
+    }
+
+    public class PersonaRepository {
+        public Persona Get(int id) {
+            return new Persona() { Id = id, Nombre = "Pepito", Apellidos = "Grillo" };
+        }
+    }
+
+}
